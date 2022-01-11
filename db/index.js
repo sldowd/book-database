05e18605ca8cde201c5b9b8a24a77ca15e477438
Sarah Dowd <sarahlynnedowd@gmail.com>
2022-01-10 21:32:51 -0500

    created DB class

---
+++ db/index.js
@@ -0,0 +1,22 @@
+const connection = require('./connection');
+
+//create ES6 class
+class DB {
+    constructor(connection) {
+        this.connection = connection;
+    }
+    getAllBooks() {
+        return this.connection.promise().query(
+            "SELECT * FROM books"
+        )
+    }
+    addBookEntry() {
+        return this.connection.promise().query(
+            `INSERT INTO books (title, author, year_finished, additional_info) VALUES ('${res.title}, '${res.author}', '${res.year}, ${res.info})`
+        )
+    }
+}
+
+const db = new DB(connection);
+
+module.exports = db;
