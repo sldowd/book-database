896fb327e83db0556c45ea3b7d40761a47381b6b
Sarah Dowd <sarahlynnedowd@gmail.com>
2022-01-20 18:33:38 -0500

    added hooks and async function to hash password on user create and update

--- models/user.js
+++ models/user.js
@@ -23,6 +23,7 @@ User.init(
             allowNull: false,
             unique: true,
             validate: {
+                //built in sequelize email validator
                 isEmail: true
             }
         },
@@ -30,16 +31,30 @@ User.init(
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
+                //regex validates that password has at least 8 characters and 
+                //contains one of each character: uppercase, lowercase, digit, special
                 is: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
                 
             }
         }
     },
-    {
+    {   
+        hooks: {
+            //set up beforeCreate hook to hash password when user is created
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+            },
+            //set up beforeUpdate hook to hash password when user is updated
+            async beforeUpdate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+            },
+        },
         //pass in imported sequelize connection
         sequelize,
+        //don't include timestamp
         timestamps: false,
         freezeTableName: true,
+        //underscored vs camelCase
         underscored: true,
         moduleName: 'user'
     }
