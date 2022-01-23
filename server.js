94033afa0dcdd5b43790d283e72fcb43a2186f13
Sarah Dowd <sarahlynnedowd@gmail.com>
2022-01-23 14:36:20 -0500

    installed handlebars engine

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = express.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3005;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //use routes
 app.use(routes);
